a5343d48cfb63c78296209b20c9c94cb8f2b3b19
Alice <alicekaradjian@gmail.com>
2024-12-18 17:29:00 -0800

    added cors

--- src/app.js
+++ src/app.js
@@ -3,11 +3,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/db");
 const setupSwagger = require("../swagger/swagger");
+const cors = require("cors")
 
 require("dotenv").config();
 
+
+
 connectDatabase();
 const app = express();
+
+app.use(cors());
+
 
 app.use("/swagger-ui", express.static(path.join(__dirname, "../node_modules/swagger-ui-dist")));
 
