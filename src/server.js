93bbe2186e49128480fa97b36fb1c0b5646bc0dd
Alice <alicekaradjian@gmail.com>
2024-12-18 17:00:31 -0800

    Added a redirect url

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.redirect("/api-docs");
+});
 
 
 // app.listen() methdos starts the server, whcih listens for incomsing reuqest on teh specific port
